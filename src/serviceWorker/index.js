744dd2edb5e0e65a56a1b42251dea6da379c3a27
qianjin.pan <qianjin.pan@dianping.com>
2018-05-09 08:53:21 +0800

    up

--- src/serviceWorker/index.js
+++ src/serviceWorker/index.js
@@ -1,12 +1,3 @@
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('./sw.js', {}).then(
-        function (registration) {
-            console.log('注册成功')
-        }
-    ).catch(function (err) {
-        console.log('注册失败')
-    })
-}
 function getImg(src) {
     return new Promise(function (resolve, reject) {
         var request = new XMLHttpRequest()
@@ -23,12 +14,51 @@ function getImg(src) {
         }
     })
 }
+function registerServiceWorker(){
+    // 注册service worker
+    return navigator.serviceWorker.register('./sw1.js').then(registration => {
+        console.log('注册成功');
+        // 返回
+        return registration;
+    })
+    .catch(err => {
+        console.error('注册失败', err);
+    });
+}
+function execute() {
+    // 允许之后执行
+    registerServiceWorker().then(registration => {
+        // 初次演示注释掉
+        registration.showNotification('Hello World!');
+    });
+}
+// 获取通知权限之后再执行
+function getPermission(){
+    return new Promise((resolve, reject) => {
+        const permissionPromise = Notification.requestPermission(result => {
+            resolve(result);
+        });
+    }).then(result => {
+            if (result === 'granted') {
+                execute();
+            }
+            else {
+                console.log('no permission');
+            }
+        });
+}
 window.onload = function () {
     var myImage = document.querySelector('#root img'),
         src = '/asset/sw.jpg';
-     myImage.src = src;
-    // getImg(src).then(res=>{
-    //     var src =  window.url.createObjectURL(res);
-    //         myImage.src = src    
-    // })
+    myImage.src = src;
+    if (!('serviceWorker' in navigator)) {
+        return;
+    }
+    if (!('PushManager' in window)) {
+        return;
+    }
+    // 初次  
+    // registerServiceWorker()
+    // 通知权限
+    getPermission()
 }
